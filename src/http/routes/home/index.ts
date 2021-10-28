4f75947793ffb923a47b39ca0af9dea82ae859d8
daangemist <git@daangemist.nl>
2021-10-28 22:56:45 +0200

    Added paging.

--- src/http/routes/home/index.ts
+++ src/http/routes/home/index.ts
@@ -2,18 +2,32 @@ import { Express } from 'express';
 import { getConfig } from '../../../config';
 import { getBragsRepository } from '../../../db';
 
+const PAGE_SIZE = 25;
+
 export default function (app: Express) {
   app.get('/', async (req, res) => {
 
     const repository = await getBragsRepository();
-    const brags = await repository.getByQuery(
-      repository.createQuery()
-        .sort('publication', 'desc')
-        .eq('published', true)
-        .limit(25)
-    );
+    const query = repository.createQuery()
+      .sort('publication', 'desc')
+      .eq('published', true)
+      .limit(PAGE_SIZE + 1);
+
+    if (req.query.offset) {
+      const afterPublished = parseInt(`${req.query.offset}`);
+      if (!isNaN(afterPublished)) {
+        query.lt('publication', afterPublished);
+      }
+    }
+
+    const brags = await repository.getByQuery(query);
+
+    let nextOffset: number|undefined = undefined;
+    if (brags.length > PAGE_SIZE) {
+      nextOffset = brags[PAGE_SIZE -1].publication
+    }
 
     // @ts-expect-error Dynamic attribute on session.
-    res.render('home', { loggedIn: req.session.loggedIn, brags, config: getConfig(), });
+    res.render('home', { loggedIn: req.session.loggedIn, brags: brags.slice(0, PAGE_SIZE), config: getConfig(), nextOffset });
   });
 }
